d4aa5b796b3adc8489591850c802fd39f1d64f41
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-10 21:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function shout(shoutText){
   return shoutText.toUpperCase();
 }
-
 function whisper(whisperText){
   return whisperText.toLowerCase();
 }
 
+
 function logShout(loggedShout){
   console.log(loggedShout.toUpperCase());
+}
+function logWhisper(loggedWhisper){
+  console.log(loggedWhisper.toUpperCase());
+}
+
+function sayHiToGrandma(input){
+  if(input )
 }
