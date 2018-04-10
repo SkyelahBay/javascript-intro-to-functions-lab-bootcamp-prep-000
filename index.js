6e15adab89f18b22739b91e435a032fb8130f6a6
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-10 21:25:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function shout(stringName){
-  return stringName.toUpperCase();
+function shout(shoutText){
+  return shoutText.toUpperCase();
+}
+
+function whisper(whisperText){
+  return whisperText.toLowerCase();
+}
+
+function logShout(loggedShout){
+  console.log(loggedShout.toUpperCase());
 }
